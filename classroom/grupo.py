fbf1c788cb191c969cbfe17053e6fef8a29e1976
jacobo0000 <jacoboleal.07@gmail.com>
2024-11-25 18:47:53 -0500

    Test 9

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,16 +3,18 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes = []):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes = None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        self._asignaturas = asignaturas if asignaturas is not None else []
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
     def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista = None):
+        if lista is None:
+            lista = []
         listaI = []
         listaI.append(alumno)
         listaI += lista
