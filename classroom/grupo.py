c9488c1f34f9a82f8d23f31c87aaeaf7280424b5
jacobo0000 <jacoboleal.07@gmail.com>
2024-11-25 18:25:01 -0500

    Test 4

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -13,8 +13,8 @@ class Grupo:
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos.append(alumno, lista)
+        # self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
         return "Grupo de estudiantes: " + self._grupo
